6633a00125dbaba1973c7266ac8f52e21282b2b8
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-13 16:36:23 +0530

    WebDriverFactory.java update

--- src/main/java/com/Vivek/Utils/WebDriverFactory.java
+++ src/main/java/com/Vivek/Utils/WebDriverFactory.java
@@ -3,23 +3,22 @@ package com.Vivek.Utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class WebDriverFactory {
-
-    private static final String CHROME_DRIVER_PATH = "path/to/chromedriver.exe";
-    private static final String GECKO_DRIVER_PATH = "path/to/geckodriver.exe";
 
     public static WebDriver createWebDriver(String browser) {
         WebDriver driver = null;
 
         switch (browser.toLowerCase()) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
                 driver = new ChromeDriver();
                 break;
             case "firefox":
-                System.setProperty("webdriver.gecko.driver", GECKO_DRIVER_PATH);
                 driver = new FirefoxDriver();
+                break;
+            case "safari":
+                driver = new SafariDriver();
                 break;
 
             default:
