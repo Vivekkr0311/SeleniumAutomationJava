efe6a393573589b84c124591e1399c9bcbd0b9c6
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-15 18:33:38 +0530

    Test Case: Home Page with three slider test case work

--- src/main/java/com/Vivek/Pages/HomePage.java
+++ src/main/java/com/Vivek/Pages/HomePage.java
@@ -1,15 +1,24 @@
 package com.Vivek.Pages;
 
 import com.Vivek.Utils.TestConfig;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage {
 
     static TestConfig config = new TestConfig();
     private static final String targetURL = config.getAccess().getProperty("WEB_URL");
+    private static final String HOME_PAGE_SHOP_BUTTON_XPATH = "//li[@id='menu-item-40']";
 
     public static WebDriver launchWebSite(WebDriver driver){
         driver.get(targetURL);
+        return driver;
+    }
+
+    public static WebDriver clickOnShopButton(WebDriver driver){
+        WebElement shopButton = driver.findElement(By.xpath(HOME_PAGE_SHOP_BUTTON_XPATH));
+        shopButton.click();
         return driver;
     }
 }
---
+++ src/test/java/com/Vivek/tests/HomePage_With_Three_Sliders_02.java
@@ -0,0 +1,44 @@
+package com.Vivek.tests;
+
+import com.Vivek.Pages.HomePage;
+import com.Vivek.Utils.TestConfig;
+import com.Vivek.Utils.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+//    1) Open the browser
+//2) Enter the URL “http://practice.automationtesting.in/”
+//            3) Click on Shop Menu
+//4) Now click on Home menu button
+//5) Test whether the Home page has Three Sliders only
+//6) The Home page must contains only three sliders
+public class HomePage_With_Three_Sliders_02 {
+
+    static WebDriver driver = null;
+    static TestConfig config = new TestConfig();
+    private static final String SAFARI = config.getAccess().getProperty("SAFARI");
+    private static final String CHROME = config.getAccess().getProperty("CHROME");
+
+    @BeforeClass
+    public void setUpMethod(){
+        driver = WebDriverFactory.createWebDriver(CHROME);
+    }
+
+    @AfterClass
+    public void cleanUpMethod(){
+        WebDriverFactory.quitWebDriver(driver);
+    }
+
+    @Test
+    public static void test() throws InterruptedException {
+        clickOnShop();
+        Thread.sleep(1000);
+    }
+
+    private static void clickOnShop(){
+        HomePage.launchWebSite(driver);
+        HomePage.clickOnShopButton(driver);
+    }
+}
