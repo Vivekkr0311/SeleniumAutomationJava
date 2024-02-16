a8a0d6919b7207c33621780a3dec7b5daf84f9db
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-16 12:43:07 +0530

    Test case two: Manual flow add

--- src/test/java/com/Vivek/tests/HomePage_With_Three_Sliders_02.java
+++ src/test/java/com/Vivek/tests/HomePage_With_Three_Sliders_02.java
@@ -8,12 +8,13 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-//    1) Open the browser
-//2) Enter the URL “http://practice.automationtesting.in/”
-//            3) Click on Shop Menu
-//4) Now click on Home menu button
-//5) Test whether the Home page has Three Sliders only
-//6) The Home page must contains only three sliders
+// Manual flow:
+// 1) Open the browser
+// 2) Enter the URL “http://practice.automationtesting.in/”
+// 3) Click on Shop Menu
+// 4) Now click on Home menu button
+// 5) Test whether the Home page has Three Sliders only
+// 6) The Home page must contains only three sliders
 public class HomePage_With_Three_Sliders_02 {
 
     static WebDriver driver = null;
