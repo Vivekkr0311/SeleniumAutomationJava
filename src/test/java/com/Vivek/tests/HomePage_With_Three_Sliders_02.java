d418b9d9bbe24a73a7d46937eee90f80af815f34
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-16 13:04:39 +0530

    Logging: Logging feature add

--- src/test/java/com/Vivek/tests/HomePage_With_Three_Sliders_02.java
+++ src/test/java/com/Vivek/tests/HomePage_With_Three_Sliders_02.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import java.util.logging.*;
 
 // Manual flow:
 // 1) Open the browser
@@ -21,36 +22,56 @@ public class HomePage_With_Three_Sliders_02 {
     static TestConfig config = new TestConfig();
     private static final String SAFARI = config.getAccess().getProperty("SAFARI");
     private static final String CHROME = config.getAccess().getProperty("CHROME");
+    private static final String WEB_BROWSER = CHROME;
+    private static final Logger logger = Logger.getLogger(Class.class.getName());
 
     @BeforeClass
     public void setUpMethod(){
-        driver = WebDriverFactory.createWebDriver(CHROME);
+        logger.log(Level.INFO, "Crating web driver " + WEB_BROWSER);
+        driver = WebDriverFactory.createWebDriver(WEB_BROWSER);
     }
 
     @AfterClass
     public void cleanUpMethod(){
+        logger.log(Level.INFO, "Cleaning up web driver " + WEB_BROWSER);
         WebDriverFactory.quitWebDriver(driver);
     }
 
     @Test
-    public static void test() throws InterruptedException {
+    public static void homePage_With_Three_Sliders_02(){
         clickOnShop();
         clickOnHomeButton();
         validationThreeSliders();
     }
 
-    private static void clickOnShop() throws InterruptedException{
-        HomePage.launchWebSite(driver);
-        HomePage.clickOnShopButton(driver);
-        Thread.sleep(1000);
+    private static void clickOnShop(){
+        logger.log(Level.INFO, "Click on shop method execution");
+        try {
+            HomePage.launchWebSite(driver);
+            HomePage.clickOnShopButton(driver);
+            Thread.sleep(1000);
+        } catch (Exception e){
+            logger.log(Level.WARNING, "Click on shop button method failed\n" + e.getStackTrace());
+        }
     }
 
-    private static void clickOnHomeButton() throws InterruptedException{
-        HomePage.clickOnHomePage(driver);
-        Thread.sleep(1000);
+    private static void clickOnHomeButton(){
+        logger.log(Level.INFO, "Click on shop method execution");
+        try{
+            HomePage.clickOnHomePage(driver);
+            Thread.sleep(1000);
+        } catch (Exception e){
+            logger.log(Level.WARNING, "Click on home button method failed\n" + e.getStackTrace());
+        }
+
     }
 
     private static void validationThreeSliders() {
-        HomePage.countSliders(driver);
+        logger.log(Level.INFO, "Three slider validation method execution");
+        try{
+            HomePage.countSliders(driver);
+        } catch (Exception e){
+            logger.log(Level.WARNING, "Three slider validation method execution\n" + e.getStackTrace());
+        }
     }
 }
