3fe105703c4c6f052973fe572a4b410324ed7dba
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-26 12:03:21 +0530

    Failing Test add

---
+++ src/test/java/com/Vivek/tests/Test_Class.java
@@ -0,0 +1,12 @@
+package com.Vivek.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test_Class {
+
+    @Test
+    public void test(){
+        Assert.assertTrue(false);
+    }
+}
